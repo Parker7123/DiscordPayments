11b9bb236ade81acee09b6748423033507ca59d8
Filip <fillipser143@gmail.com>
2022-05-27 01:00:09 +0200

    domain handling shop handling not ready token safety

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,20 +12,27 @@ import org.javacord.api.entity.message.component.ActionRow;
 import org.javacord.api.entity.message.component.Button;
 import org.javacord.api.interaction.MessageComponentInteraction;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Properties;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchFieldException, SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws NoSuchFieldException, SQLException, ClassNotFoundException, IOException {
         var database = new MainDatabase();
+        String appConfigPath = Thread.currentThread().getContextClassLoader().getResource("token.properties").getPath();
+        System.out.println(appConfigPath);
+        Properties appProps = new Properties();
+        appProps.load(new FileInputStream(appConfigPath));
         DiscordPaymentBot discordPaymentBot =
-                new DiscordPaymentBot("ODQxMzkzNzU4ODAwMDUyMjg0.YJmHAA.g1Z60YLU2zMgX_Q4b-kmrbouiMc",
-                        new BuyButtonDmProcessor());
+                new DiscordPaymentBot(appProps.getProperty("token"), new BuyButtonDmProcessor());
 
         discordPaymentBot.sendPaymentMessage("855424831745818685","buy the bot");
     }
